c1a2d9b063026f1c2ef445d926c97a06f346f4df
John Conrad <addguitarist@yahoo.com>
2023-04-27 13:27:11 -0500

    blind view

---
+++ Presonus.UCNet.Wpf.Blind/App.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Presonus.UCNet.Api;
+using Presonus.UCNet.Api.Helpers;
+using Presonus.UCNet.Api.Models;
+using Presonus.UCNet.Api.Services;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Presonus.UCNet.Wpf.Blind
+{
+	public partial class App : Application
+	{
+		public static ServiceProvider ServiceProvider { get; set; }
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+#if DEBUG
+			//Log.Logger = new LoggerConfiguration()
+			//	.WriteTo.Console()
+			//	.CreateLogger();
+
+			AllocConsole();
+#endif
+
+			var serviceCollection = new ServiceCollection();
+
+			// Register the new classes and their dependencies
+			serviceCollection.AddSingleton<MixerState>();
+			serviceCollection.AddSingleton<MixerStateTraverser>();
+			serviceCollection.AddSingleton<MixerStateSynchronizer>();
+			serviceCollection.AddSingleton<MixerStateService>();
+
+			serviceCollection.AddSingleton<BroadcastService>();
+			serviceCollection.AddSingleton<CommunicationService>();
+			serviceCollection.AddSingleton<MeterService>();
+
+
+			// WPF UI
+			serviceCollection.AddSingleton<BlindViewModel>();
+			serviceCollection.AddSingleton<MainWindow>();
+
+			ServiceProvider = serviceCollection.BuildServiceProvider();
+
+			ServiceProvider.GetRequiredService<BroadcastService>().StartReceive();
+			while (!Mixer.Counted)
+			{
+				Task.Delay(100).Wait();
+			}
+
+			// Run application:
+			var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+			mainWindow.Show();
+		}
+
+		[System.Runtime.InteropServices.DllImport("kernel32.dll")]
+		internal static extern bool AllocConsole();
+	}
+}
---
+++ Presonus.UCNet.Wpf.Blind/BlindViewModel.cs
@@ -0,0 +1,148 @@
+﻿using Presonus.UCNet.Api.Models;
+using Presonus.UCNet.Api.Models.Channels;
+using Presonus.UCNet.Api.Services;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows.Threading;
+
+namespace Presonus.UCNet.Api;
+
+public class BlindViewModel : INotifyPropertyChanged
+{
+	private MixerStateService _mixerStateService;
+	private MeterService _meterService;
+	private DispatcherTimer updateTimer;
+
+	public TreeNode Tree { get; set; }
+
+
+	public BlindViewModel(MixerStateService mixerStateService, MeterService meterService)
+	{
+		_mixerStateService = mixerStateService;
+		Presets = new Presets(mixerStateService);
+		Presets.PropertyChanged += (sender, args) => OnPropertyChanged(nameof(Presets));
+
+		Global = new Global(mixerStateService);
+		Global.PropertyChanged += (sender, args) => OnPropertyChanged(nameof(Global));
+
+		for (int i = 0; i < Mixer.ChannelCounts[ChannelTypes.LINE]; i++)
+		{
+			var chan = new MicLineInput(ChannelTypes.LINE, i + 1, _mixerStateService);
+			chan.PropertyChanged += (sender, args) => OnPropertyChanged(nameof(MicLineInputs));
+			MicLineInputs.Add(chan);
+			AllChannels.Add(chan);
+		}
+
+		for (int i = 0; i < Mixer.ChannelCounts[ChannelTypes.FXRETURN]; i++)
+		{
+			var chan = new InputChannel(ChannelTypes.FXRETURN, i + 1, _mixerStateService);
+			chan.PropertyChanged += (sender, args) => OnPropertyChanged(nameof(FXReturns));
+			FXReturns.Add(chan);
+			AllChannels.Add(chan);
+		}
+
+		for (int i = 0; i < Mixer.ChannelCounts[ChannelTypes.RETURN]; i++)
+		{
+			var chan = new StereoLineInput(i + 1, _mixerStateService);
+			chan.PropertyChanged += (sender, args) => OnPropertyChanged(nameof(StereoLineInputs));
+			StereoLineInputs.Add(chan);
+			AllChannels.Add(chan);
+		}
+
+		for (int i = 0; i < Mixer.ChannelCounts[ChannelTypes.TALKBACK]; i++)
+		{
+			var chan = new MicLineInput(ChannelTypes.TALKBACK, i + 1, _mixerStateService);
+			chan.PropertyChanged += (sender, args) => OnPropertyChanged(nameof(Talkback));
+			Talkback.Add(chan);
+			AllChannels.Add(chan);
+		}
+
+		for (int i = 0; i < Mixer.ChannelCounts[ChannelTypes.MAIN]; i++)
+		{
+			var chan = new OutputDACBus(ChannelTypes.MAIN, i + 1, _mixerStateService);
+			chan.PropertyChanged += (sender, args) => OnPropertyChanged(nameof(Main));
+			Main.Add(chan);
+			AllChannels.Add(chan);
+		}
+
+		updateTimer = new DispatcherTimer
+		{
+			Interval = TimeSpan.FromMilliseconds(10) // Update every 100 milliseconds
+		};
+		updateTimer.Tick += UpdateTimer_Tick;
+		updateTimer.Start();
+
+
+		Tree = new();
+		PopulateTree(Tree, this);
+	}
+	public void PopulateTree(TreeNode node, object obj)
+	{
+		Type type = obj.GetType();
+
+		foreach (PropertyInfo prop in type.GetProperties())
+		{
+			var propNode = new TreeNode
+			{
+				Name = prop.Name,
+				Value = prop.GetValue(obj)
+			};
+
+			node.Children.Add(propNode);
+
+			if (prop.PropertyType.IsClass && prop.PropertyType != typeof(string))
+			{
+				PopulateTree(propNode, prop.GetValue(obj));
+			}
+		}
+	}
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	public MeterData MeterData { get; set; }
+	public ReductionMeterData ReductionMeterData { get; set; }
+	public ObservableCollection<MicLineInput> MicLineInputs { get; } = new ObservableCollection<MicLineInput>();
+
+	public ObservableCollection<StereoLineInput> StereoLineInputs { get; } = new ObservableCollection<StereoLineInput>();
+
+	public ObservableCollection<InputChannel> FXReturns { get; } = new ObservableCollection<InputChannel>();
+
+	public ObservableCollection<MicLineInput> Talkback { get; } = new ObservableCollection<MicLineInput>();
+
+	public ObservableCollection<OutputDACBus> Main { get; } = new ObservableCollection<OutputDACBus>();
+
+	public ObservableCollection<Channel> AllChannels { get; } = new ObservableCollection<Channel>();
+
+	public Presets Presets { get; }
+
+	public Global Global { get; }
+
+	private void UpdateTimer_Tick(object sender, EventArgs e)
+	{
+		// OnPropertyChanged(nameof(MeterData.Input));
+	}
+
+	protected virtual void OnPropertyChanged(string propertyName)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		//if (propertyName == "MeterData") Console.WriteLine(MeterData.Input[0]);
+	}
+
+	public void Test()
+	{
+		_mixerStateService.RequestProjects();
+		// _mixerStateService.RecallSceneFile("02.test.proj", "02.testing.scn");
+	}
+}
+public class TreeNode
+{
+	public string Name { get; set; }
+	public object Value { get; set; }
+	public ObservableCollection<TreeNode> Children { get; set; }
+
+	public TreeNode()
+	{
+		Children = new ObservableCollection<TreeNode>();
+	}
+}
